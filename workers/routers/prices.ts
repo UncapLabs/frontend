038e0adce5c87e2cbbc94e39e6fa8bf81f64decc
Maxime Servais <maximeservais@hotmail.com>
2025-07-15 16:55:04 +0200

    update deploy addresses

--- workers/routers/prices.ts
+++ workers/routers/prices.ts
@@ -4,8 +4,9 @@ import { getBitcoinprice } from "workers/services/utils";
 export const priceRouter = router({
   getBitcoinPrice: publicProcedure.query(async ({ ctx }) => {
     const price = await getBitcoinprice();
+    const rawPrice = price[0];
     return {
-      price: Number(price) / 1e18,
+      price: Number(rawPrice) / 1e18,
     };
   }),
   getBitUSDPrice: publicProcedure.query(({ ctx }) => {
