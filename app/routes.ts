ac84dc4eddafb2ae91b22319e2d34001af8d71d2
Maxime Servais <maximeservais@hotmail.com>
2026-01-19 14:51:40 +0100

    vault analytics

--- app/routes.ts
+++ app/routes.ts
@@ -28,6 +28,7 @@ export default [
     // route("leaderboard", "routes/points/leaderboard.tsx"),
     route("claim", "routes/claim/claim.tsx"),
     route("stats", "routes/stats.tsx"),
+    route("vault", "routes/vault/vault.tsx"),
     route("terms-and-conditions", "routes/legal/terms.tsx"),
     route("privacy-policy", "routes/legal/privacy.tsx"),
     route("mint-btc", "routes/admin.mint-btc.tsx"),
